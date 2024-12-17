5ee747a53c2d8327d9308ad4b6389da6ae55d488
Tri <105915392+TriFoxie@users.noreply.github.com>
2024-12-17 11:18:03 +0000

    Simplify changeHealth proc

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -7,24 +7,16 @@ public class Entity : MonoBehaviour
 {
     [Header("Entity Stats")]
     public float health { get; set{ ChangeHealth(value); }}
-    [SerializeField] private UnityEvent Take1Damage;
+    [SerializeField] private 
 
     [Header("Core Stats")]
     public float speed { get; protected set; }//Movement speed etc
     public float strength {get; protected set;}//Melee multiplier
     public float Distance {get; protected set;}//Distance stat
 
-    public IEnumerator ChangeHealth(float amount){
-        while(amount > 0){
-            yield return new WaitForSeconds(0.5f);
-            try { take1Damage.Invoke(); }
-            catch (exception e){
-                switch(e){
-                    case ""/*null reference exception*/: Debug.Log("No change health function applied"); break;
-                    default: Debug.Log(e);
-                }
-            }
-        }
+    public void ChangeHealth(float amount){
+        Debug.Log("Taking " + amount + " damage!");
         health = amount;
+        Debug.Log("Health: " + this.health);
     }
 }
