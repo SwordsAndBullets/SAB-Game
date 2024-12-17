7ef8c74f60bf1252a6b986db0cd5e9a62189454b
Tri <105915392+TriFoxie@users.noreply.github.com>
2024-12-17 10:59:30 +0000

    Basic Entity class

--- Assets/Entity.cs
+++ Assets/Entity.cs
@@ -1,30 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class Entity : MonoBehaviour
 {
-    #region[data]
-    [Header("----Stats----")]
-    public int level;
-    public int health;
+    [Header("Entity Stats")]
+    public float health { get; set{ ChangeHealth(value); }}
+    [SerializeField] private UnityEvent Take1Damage;
 
-    [Header("----Abilities----")]
-    public int ability1Charge;//On player :: Grenade
-    public int ability2Charge;//On player :: Melee
+    [Header("Core Stats")]
+    public float speed { get; protected set; }//Movement speed etc
+    public float strength {get; protected set;}//Melee multiplier
+    public float Distance {get; protected set;}//Distance stat
 
-    [Header("----Ability Recharges----")]
-    public int strength; //melee cooldown
-    public int agility; //speed + reload speed
-    public int armament; //grenade cooldown
-
-    [Header("----Keywords----")]
-    public bool burning;
-    public bool weakened;
-    public bool frozen;
-    #endregion
-
-    [Header("----References----")]
-    private Item[] armourItems;
-    private Item[] weaponItems;
+    public IEnumerator ChangeHealth(float amount){
+        while(amount > 0){
+            yield return new WaitForSeconds(0.5f);
+            try { take1Damage.Invoke(); }
+            catch (exception e){
+                switch(e){
+                    case ""/*null reference exception*/: Debug.Log("No change health function applied"); break;
+                    default: Debug.Log(e);
+                }
+            }
+        }
+        health = amount;
+    }
 }
